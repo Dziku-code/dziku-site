c65efec2e699e1b043e06b0f5319df4d2c9f07b6
Dariusz Pazdur <pazdur.dariusz17@gmail.com>
2024-04-11 16:45:20 +0200

    Set up states for menu

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 
 const links = [
   { url: "/", title: "HOME" },
@@ -8,10 +11,22 @@ const links = [
 ];
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      <div className="hidden md:flex gap-4">
+      {links.map((link) => (
+              <a
+                href={link.url}
+                key={link.title}
+                className="text-white no-underline hover:underline"
+              >
+                {link.title}
+              </a>
+      ))}
+      </div>
       {/* LOGO */}
-      <div className="">
+      <div className="md:hidden">
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-mono font-semibold flex items-center justify-center"
@@ -23,22 +38,30 @@ const Navbar = () => {
         </Link>
       </div>
       {/* Responsive Menu */}
-      <div className="">
-         {/* Menu Button */}
-        <button className="w-10 h-8 flex flex-col justify-between z-50">
-          <div className="w-10 h-1 bg-black rounded"></div>
-          <div className="w-10 h-1 bg-black rounded"></div>
-          <div className="w-10 h-1 bg-black rounded"></div>
+      <div className="md:hidden">
+        {/* Menu Button */}
+        <button
+          className="w-10 h-8 flex flex-col justify-between z-50 relative"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
         </button>
-        
         {/* Menu List */}
-        <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex items-center flex-col justify-center gap-8 text-4xl ">
-          {links.map((link) => (
-            <Link href={link.url} key={link.title}>
-              {link.title}
-            </Link>
-          ))}
-        </div>
+        {open && (
+          <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex items-center flex-col justify-center gap-8 text-4xl ">
+            {links.map((link) => (
+              <a
+                href={link.url}
+                key={link.title}
+                className="text-white no-underline hover:underline"
+              >
+                {link.title}
+              </a>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
