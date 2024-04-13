73d9a7ba3d13fd7ca50f609557bda1c8fbf8e8eb
Dariusz Pazdur <pazdur.dariusz17@gmail.com>
2024-04-13 23:09:46 +0200

    End home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 const Homepage = () => {
   return (
-    <div className="h-full flex flex-col bg-white-500">
+    <div className="h-full flex flex-col bg-white-500 px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 lg:flex-row lg:gap-8 ">
       {/* IMAGE CONTAINER - Assuming you want the image to fill half the height of the screen */}
-      <div className="relative h-1/2">
+      <div className="relative h-1/2 lg:h-full lg:w-1/2 relative lg:flex-row ">
         <Image
           src="/dziku_page.png"
           alt="boar image"
@@ -13,20 +13,20 @@ const Homepage = () => {
         />
       </div>
       {/* TEXT CONTAINER */}
-      <div className="h-1/2 flex flex-col gap-8 items-center justify-center">
+      <div className="h-1/2 flex flex-col gap-8 items-center justify-center lg:h-full lg:w-1/2 ">
         {/* Title */}
-        <h1 className="text-4xl font-bold"> Forging a web's masterpieces</h1>
+        <h1 className="text-4xl md:text-6xl font-bold "> Forging a web's masterpieces</h1>
         {/* DESC */}
-        <p className="">
+        <p className="md:text-xl">
           {" "}
           *Growl* Hi! Take a look of my work as full stack developer, digital
           artist and junior game creator *Growl*{" "}
         </p>
-        <div className="flex gap-4">
+        <div className="flex gap-4 lg:w-full md:justify-content">
           <button className=" p-4 rounded-lg ring-1 ring-black bg-black text-white">
             View my work
           </button>
-          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+          <button className="p-4 rounded-lg ring-1 ring-black bg-white text-black">
             Contact me
           </button>
         </div>
