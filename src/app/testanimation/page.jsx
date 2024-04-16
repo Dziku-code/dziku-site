a25ca42c585e7fa8e5590e6eea6778a65371b1d8
Dariusz Pazdur <pazdur.dariusz17@gmail.com>
2024-04-16 20:38:37 +0200

    testing animations

--- src/app/testanimation/page.jsx
+++ src/app/testanimation/page.jsx
@@ -1,15 +1,29 @@
-'use client';
+"use client";
 
 import { motion } from "framer-motion";
 
 const TestPage = () => {
+  const variants = {
+    variant1: {
+      x: 400,
+      y: 200,
+      opacity: 0.5,
+    },
+    variant2: {
+      x: 100,
+      y: -300,
+      opacity: 0.5,
+    },
+  };
+
   return (
     <div className="h-full flex items-center justify-center">
       <motion.div
         className="w-96 h-96 bg-red-400 rounded"
         initial={{ x: -100 }}
-        animate={{ x: 100, y: 300, opacity:0.5}}
-      />
+        variants={variants}
+        animate='variant2'
+      ></motion.div>
     </div>
   );
 };
